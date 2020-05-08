71d316f94e67788eedc7e098b78c6a3fe22f2811
dhu16 <44564696+dhu16@users.noreply.github.com>
2020-05-07 22:10:04 -0700

    Add files via upload

---
+++ Complex.cpp
@@ -0,0 +1,147 @@
+#include "Complex.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+Complex::Complex(std::string& input) //string constructor
+{
+	std::istringstream ss1(input);
+	std::string numHolder;
+	std::string firstChunk;
+
+	if (!ss1) //if stringstream read fails
+	{
+		ss1.clear(); //clear
+		throw std::logic_error("ERROR!\n"); //throw error
+	}
+	else {
+		while (!ss1.eof()) //while ss1 is not EOF
+		{
+			getline(ss1, firstChunk, '+'); //gets first part of complex number until the + sign and stores in ss1
+			
+			std::istringstream ss2(firstChunk); //creating second stringstream
+
+			if (!ss2) //if ss2 fails
+			{
+				ss2.clear();
+				throw std::logic_error("ERROR!\n"); //throws exception
+			}
+			else 
+			{
+				while (ss2) //till end of ss2
+				{
+					while (ss2.peek() != '-') //while next character is not - sign
+					{
+						ss2 >> numHolder; //store characters into numHolder string
+					}
+
+					if (!ss2.eof()) //if ss2 is not at the end
+					{
+						numHolder.push_back(ss2.get()); //push back character to numHolder
+					}
+
+					if (numHolder.find('i') == std::string::npos) //if the character is real
+					{
+						real += std::stod(numHolder); //converts numHolder to double and adds to real
+					}
+					else if (numHolder.find('i') && (numHolder.size() == 1)) //if character is imaginary
+					{
+						++imag; //increments imag
+					}
+					else if (numHolder.find("-i")) //if it finds -i 
+					{
+						--imag; //decrements imag;
+					}
+					else //if some number and i
+					{
+						imag += std::stod(numHolder); //converts numHolder to double and adds to imag
+					}
+				}
+			}
+		}
+	}
+}
+
+double& Complex::operator[](const std::string& str) 
+{
+	if (str == "real") //if string holds real
+	{
+		double& refreal = real; //create reference to real
+		return refreal; //return reference
+	}
+	else if (str == "imag")
+	{
+		double& refimag = imag;
+		return refimag; //return reference to imag
+	}
+	else
+	{
+		throw std::out_of_range("invalid index: " + str); //throws exception otherwise
+	}
+}
+
+const double Complex::operator[](const std::string& str) const //overloaded on const version
+{
+	if (str == "real") 
+	{
+		return real; //returns member variable real
+	}
+	else if (str == "imag") 
+	{
+		return imag; //returns imag
+	}
+	else 
+	{
+		throw std::out_of_range("invalid index: " + str);
+	}
+}
+
+std::ostream& operator<<(std::ostream& out, const Complex& num) 
+{
+	if (num["imag"] == 1) //if imaginary number is 1
+	{
+		out << num["real"] << "i"; //returns real number and i
+	}
+	else if (num["imag"] == -1) //if imag is -1
+	{
+		out << num["real"] << "-i"; //returns real and -i
+	}
+	else if (num["imag"] > 0) //if imag > 0
+	{
+		out << num["real"] << "+" << num["imag"] << "i"; //returns real, imag, i
+	}
+	else if(num["imag"] < 0)
+	{
+		out << num["real"] << num["imag"] << "i";
+	}
+
+	if (num["real"] == 0) 
+	{
+		out << "0"; //display empty constructor
+	}
+
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, Complex& num) 
+{
+	in >> num.real; //puts input into real
+	std::cout << " ";
+	in >> num.imag; //puts input into imag
+
+	if (in.fail()) //if istream in fails throw exception
+	{
+		throw std::runtime_error("read error");
+	}
+
+	return in;
+}
+
+//conversion to string operator
+Complex::operator std::string() const
+{
+	std::ostringstream oss; //creates ostringstream
+	oss << *this; //outs to *this
+	return oss.str(); //returns string version of oss
+}
---
+++ Complex.h
@@ -0,0 +1,397 @@
+#ifndef COMPLEX_
+#define COMPLEX_
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
+class Complex //MY CODE ONLY WORKS UP UNTIL THE STRING CONSTRUCTOR IS TESTED WHICH IS AT THE SOURCE MAIN CPP LINE 89
+{
+
+private: 
+
+	double real, imag; //doubles
+
+public:
+
+	/**
+	Default constructor with real and imag as arguments
+	that are initialized to 0.
+	@param _real : real number
+	@param _imag : imaginary number
+	*/
+	constexpr Complex(double _real = 0, double _imag = 0) : real(_real), imag(_imag) {}
+
+	/**
+	String constructor that takes string ref input
+	and converts it into a complex number
+	@param input : string input
+	*/
+	Complex(std::string& input); 
+
+	/**
+	Overloaded constexpr operator += that adds 2 complex
+	numbers and changes left side
+	@param num : right hand side complex num
+	@return *this : the left side
+	*/
+	constexpr Complex& operator+=(const Complex& num)
+	{
+		real += num.real;
+		imag += num.imag;
+		return *this;
+	}
+
+	/**
+	Overloaded operator + that creates
+	copy of Complex object
+	@return copy : copy of complex num
+	*/
+	constexpr Complex operator+() const
+	{
+		Complex copy(*this); //creates copy
+		copy.real = real;
+		copy.imag = imag;
+		return copy;
+	}
+
+	/**
+	Overloaded unary prefix operator ++ that
+	increments real number
+	@return *this : real number
+	*/
+	constexpr Complex& operator++()
+	{
+		++real; //increments
+		return *this;
+	}
+
+	/**
+	Overloaded binary postfix operator ++ that
+	increments real num using a copy
+	@param unused : unused int
+	@return copy : return incremented copy
+	*/
+	constexpr Complex operator++(int unused)
+	{
+		Complex copy(*this);
+		++copy.real;
+		return copy;
+	}
+
+	/**
+	Overloaded operator -= to subtract two
+	complex numbers and change left side
+	@param num : right side
+	@return *this : left side
+	*/
+	constexpr Complex& operator-=(const Complex& num)
+	{
+		real -= num.real;
+		imag -= num.imag;
+
+		return *this;
+	}
+
+	/**
+	Overloaded operator - that negates real
+	and imag parts using copy of complex num
+	@return copy : negated copy
+	*/
+	constexpr Complex operator-() const
+	{
+		Complex copy(*this);
+		copy.real = -real; //negated real
+		copy.imag = -imag;
+		return copy;
+	}
+
+	/**
+	Overloaded unary prefix operator -- that
+	decrements real part
+	@return *this : decremented complex num
+	*/
+	constexpr Complex& operator--()
+	{
+		--real; //decrement
+		return *this;
+	}
+
+	/**
+	Overloaded binary postfix operator -- that
+	decrements real part using copy of complex
+	num
+	@return copy : decremented copy
+	*/
+	constexpr Complex operator--(int unused)
+	{
+		Complex copy(*this);
+		--real;
+		return copy;
+	}
+
+	/**
+	Overloaded operator *= that multiples complex
+	nums and changes left side
+	@param num : right side
+	@return *this : left side
+	*/
+	constexpr Complex& operator*=(const Complex& num)
+	{
+		real = (real * num.real) - (imag * num.imag); //using complex number rules to multiply
+		imag = (imag * num.real) + (real * num.imag);
+
+		return *this;
+	}
+
+	/**
+	Overloaded operator /= that divides 2 complex
+	nums and changes left side
+	@param num : right side
+	@return *this : left side
+	*/
+	constexpr Complex& operator/=(const Complex& num)
+	{
+		real = ((real * num.real) + (imag * num.imag)) / ((num.real * num.real) + (num.imag * num.imag)); //divides following rules
+		imag = ((imag * num.real) - (real * num.imag)) / ((num.real * num.real) + (num.imag * num.imag));
+
+		return *this;
+	}
+
+	/**
+	Overloaded unary operator~ that returns
+	conjugate of complex nums
+	@return Complex(real, -imag) : conjugate
+	*/
+	constexpr Complex operator~() const
+	{
+		return Complex(real, -imag); //returns conjugate
+	}
+
+	/**
+	Call operator taking no arguments that sets complex
+	number to 0
+	@return *this : complex num set to 0
+	*/
+	constexpr Complex& operator()() 
+	{
+		real = 0;
+		imag = 0;
+
+		return *this;
+	}
+
+	/**
+	Subscript operator taking string argument
+	and returning either real or imag references.
+	Throws exception if invalid
+	@param str : real or imag input
+	@return refreal, refimag : references to real/imag
+	*/
+	double& operator[](const std::string& str);
+
+	/**
+	Subscript operator overloaded on const
+	that takes real/imag input and returns
+	member variables respectively.
+	Throws exception if invalid
+	@param str : real/imag input
+	@return real, imag : member variables
+	*/
+	const double operator[](const std::string& str) const;
+
+	/**
+	Overloaded friend operator >> that reads 2
+	doubles and sets real and imag to them 
+	of an input stream. Throws exception if istream
+	fails
+	@param in : istream in object
+	@param num : complex num
+	@return in : returns istream obj
+	*/
+	friend std::istream& operator>>(std::istream& in, Complex& num);
+
+	/**
+	Overloaded conversion operator that converts
+	Complex object to std::string
+	@return oss.str() : string version of ostringstream that is the Complex object
+	*/
+	operator std::string() const;
+
+};
+
+/**
+Operator + that adds 2 complex nums
+and returns sum
+@param lnum : left side
+@param rnum : right side
+@return sum
+*/
+constexpr Complex operator+(Complex lnum, const Complex& rnum)
+{
+	return lnum += rnum;
+
+}
+
+/**
+Operator - that calculates difference
+of 2 complex nums
+@param lnum : left side
+@param rnum : right side
+@return difference
+*/
+constexpr Complex operator-(Complex lnum, const Complex& rnum)
+{
+	return lnum -= rnum;
+}
+
+/**
+Operator * calculates product of 2 complex
+nums
+@param lnum : left side
+@param rnum : right side
+@return product
+*/
+constexpr Complex operator*(Complex lnum, const Complex& rnum)
+{
+	return lnum *= rnum;
+}
+
+/**
+Operator / divides 2 complex nums
+@param lnum : left side
+@param rnum : right side
+@return quotient
+*/
+constexpr Complex operator/(Complex lnum, const Complex& rnum)
+{
+	return lnum /= rnum;
+}
+
+/**
+Operator << that displays complex num
+with output stream properly
+@param out : ostream object
+@param num : complex num
+@return out : printed object
+*/
+std::ostream& operator<<(std::ostream& out, const Complex& num);
+
+/**
+Comparison operator < comparing left
+and right side
+@param left : left side complex num
+@param right : ride side
+@return : comparison
+*/
+constexpr bool operator<(const Complex& left, const Complex& right)
+{
+	if (left < right)
+	{
+		return !(left < right);
+	}
+
+	return false;
+}
+
+/**
+Comparison operator == comparing left
+and right side if they have the same values
+@param left : left side complex num
+@param right : ride side
+@return : comparison
+*/
+constexpr bool operator==(const Complex& left, const Complex& right)
+{
+	return !((left < right) || (right < left));
+}
+
+/**
+Comparison operator > comparing left
+and right side
+@param left : left side complex num
+@param right : ride side
+@return : comparison
+*/
+constexpr bool operator>(const Complex& left, const Complex& right)
+{
+	if (left > right) //if true
+	{
+		return !(left > right);
+	}
+	else //if false
+	{
+		return false;
+	}
+}
+
+/**
+Comparison operator <= comparing left
+and right side
+@param left : left side complex num
+@param right : ride side
+@return : comparison if true
+*/
+constexpr bool operator<=(const Complex& left, const Complex& right)
+{
+	if (left <= right) //if true
+	{
+		return !(left <= right);
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+/**
+Comparison operator >= comparing left
+and right side
+@param left : left side complex num
+@param right : ride side
+@return : comparison if true
+*/
+constexpr bool operator>=(const Complex& left, const Complex& right)
+{
+	if (left >= right) //if true
+	{
+		return !(left >= right);
+	}
+	else //else
+	{
+		return false;
+	}
+}
+
+/**
+Comparison operator != comparing left
+and right side for inequality
+@param left : left side complex num
+@param right : ride side
+@return : comparison if true
+*/
+constexpr bool operator!=(const Complex& left, const Complex& right)
+{
+	if (left != right) //if does not equal
+	{
+		return !(left != right);
+	}
+	else 
+	{
+		return false; 
+	}
+}
+
+/**
+User-defined literal expression that converts
+double into Complex number
+@param input : long double input
+@return Complex(0.0, static_cast<double>(input)) : returned Complex obj
+*/
+constexpr Complex operator "" _i(long double input)
+{
+	return Complex(0.0, static_cast<double>(input)); //static casts
+}
+
+#endif
